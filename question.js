e4b7206ff26ed4faf296352fe6c016bb34b19e75
Paul <kun@example.com>
2025-04-22 17:42:45 +0800

    Added instructions

--- question.js
+++ question.js
@@ -6,4 +6,27 @@
  * @method checkAnswer(answerObject) - Check if a user's answer is correct
  */
 
+class AbstractQuestion {
+    constructor(question, answer, score) {
 
+    }
+    render() {
+
+    }
+    checkAnswer(answerObject) {
+        return false;
+    }
+}
+
+class ShortAnswerQuestion extends AbstractQuestion {
+    constructor(question, answer, score) {
+        super(question, answer, score);
+    }
+    render() {
+        displayText(this.question);
+        displayTextbox('Answer', '');
+    }
+    checkAnswer(answerObject) {
+        return answerObject.answer === this.answer;
+    }
+}
