e653472a03d5471550ecc1e9f46678a41b551a64
Paul <kun@example.com>
2025-04-22 17:34:07 +0800

    Added abtract question

--- question.js
+++ question.js
@@ -5,5 +5,15 @@
  * @method render() - Render the question in the DOM
  * @method checkAnswer(answerObject) - Check if a user's answer is correct
  */
-
+class AbstractQuestion {
+    constructor(question, answer, score) {
+       
+    }
+    render() {
+       
+    }
+    checkAnswer(answerObject) {
+        return false;
+    }
+}
 
